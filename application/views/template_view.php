c5eea36c2780bcffa5a1237296813d527cbca47f
matejka9 <dusan.matejka.dm@gmail.com>
2016-11-29 23:09:50 +0100

    title fix

--- application/views/template_view.php
+++ application/views/template_view.php
@@ -2,13 +2,12 @@
 <html lang="en" xmlns="http://www.w3.org/1999/xhtml">
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-
 <script src="<?= base_url()?>assets/bootstrap/js/bootstrap.min.js"></script>
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title><?= $title ?></title>
+	<!-- <title><?= $title ?></title> -->
 	<link href='http://fonts.googleapis.com/css?family=Bree+Serif' rel='stylesheet' type='text/css'>
 	<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
 	<link href="<?= base_url()?>assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
