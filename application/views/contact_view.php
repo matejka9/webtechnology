32992422ceb6850ecd8931c8a12eb0f6daf15f74
matejka9 <dusan.matejka.dm@gmail.com>
2016-11-29 22:10:57 +0100

    Contact and menu

--- application/views/contact_view.php
+++ application/views/contact_view.php
@@ -3,6 +3,55 @@
 <link href="<?= base_url()?>assets/css/style-contact-big.css" rel="stylesheet" media="only screen and (min-width: 1001px)">
 
 <script type="text/javascript">
+
+$(window).load(function(){
+	var w = window.innerWidth;
+    var h = window.innerHeight;
+    console.log(w);
+    console.log(h);
+    if (w < 520){
+    	$("#mapaGoogle").hide();
+    } else {
+    	$("#mapaGoogle").show();
+    }
+});
+
+
+$(window).resize(function(){
+	var w = window.innerWidth;
+    var h = window.innerHeight;
+
+    if (w < 520){
+    	$("#mapaGoogle").hide();
+    } else {
+    	$("#mapaGoogle").show();
+    }
+});
+
+//     var w = window.innerWidth;
+// 	var small_w = $("#contactText").width();
+
+// 	//console.log(w);
+// 	//console.log(small_w);
+
+// 	var leftMargin = (w - small_w) / 2;
+// 	$("#contactText").css("position","absolute" );
+// 	$("#contactText").css("left",leftMargin );
+// 	$("#contactText").css("top","20%" );
+
+// 	var rect = $("#contactText").getBoundingClientRect();
+
+// 	$("#mapaGoogle").css("position","absolute" );
+// 	$("#mapaGoogle").css("left", "10px");
+
+
+// 	$("#mapaGoogle").css("top", rect.bottom);
+
+
+// });
+
+
+
 $(function(){
    $(".nav").find(".active").removeClass("active");
    $("#contact").parent().addClass("active");
@@ -15,10 +64,10 @@ $(function(){
 
 <script src='https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyCCObpQl1lf3xSirBupKY_6tXuBrTz1r0w'></script>
 <div class="contact_page">
-    <div style="max-width:520px; width:100%;">
-		<div>
+    <div class="first" style="max-width:520px; width:100%;">
+		<div id="contactText" >
 			<h3>Restaurant Tutorial</h3>
-			<table class="first w3-table w3-striped w3-bordered">
+			<table class="w3-table w3-striped w3-bordered">
 		        <tr>
 		          <th>Location</th>
 		          <td>Nedbalova 10</td>
@@ -42,17 +91,18 @@ $(function(){
 			</table>
 		</div>
 		<br>
-		<div class= "second">
+		<!-- <div class= "second" id="mapaGoogle">
 			<div style='overflow:hidden;height:400px;width:520px;'>
 				<div id='gmap_canvas' style='height:400px;width:520px;'></div>
 				<style>#gmap_canvas img{max-width:none!important;background:none!important}</style>
 			</div> <a href='http://maps-website.com/'>www.Maps-website.com</a> 
-		</div>
+		</div> -->
 	</div>
 </div>
 
-<script type='text/javascript' src='https://embedmaps.com/google-maps-authorization/script.js?id=4c65c7bd8843a7a7357017fa4a4a65fb8506dd59'></script>
-<script type='text/javascript'>function init_map(){var myOptions = {zoom:17,center:new google.maps.LatLng(48.1442093,17.11099809999996),mapTypeId: google.maps.MapTypeId.ROADMAP};map = new google.maps.Map(document.getElementById('gmap_canvas'), myOptions);marker = new google.maps.Marker({map: map,position: new google.maps.LatLng(48.1442093,17.11099809999996)});infowindow = new google.maps.InfoWindow({content:'<strong>Tutorial Restaurant</strong><br>Nedbalova 10<br>811 01 Bratislava 1<br>'});google.maps.event.addListener(marker, 'click', function(){infowindow.open(map,marker);});infowindow.open(map,marker);}google.maps.event.addDomListener(window, 'load', init_map);</script>
+<!-- <script type='text/javascript' src='https://embedmaps.com/google-maps-authorization/script.js?id=4c65c7bd8843a7a7357017fa4a4a65fb8506dd59'></script>
+<script type='text/javascript'>
+function init_map(){var myOptions = {zoom:17,center:new google.maps.LatLng(48.1442093,17.11099809999996),mapTypeId: google.maps.MapTypeId.ROADMAP};map = new google.maps.Map(document.getElementById('gmap_canvas'), myOptions);marker = new google.maps.Marker({map: map,position: new google.maps.LatLng(48.1442093,17.11099809999996)});infowindow = new google.maps.InfoWindow({content:'<strong>Tutorial Restaurant</strong><br>Nedbalova 10<br>811 01 Bratislava 1<br>'});google.maps.event.addListener(marker, 'click', function(){infowindow.open(map,marker);});infowindow.open(map,marker);}google.maps.event.addDomListener(window, 'load', init_map);</script> -->
 
 
-<div id="map"></div>
+<!-- <div id="map"></div> -->
--- application/views/home_view.php
+++ application/views/home_view.php
@@ -9,12 +9,22 @@ $(function(){
    $("#home").parent().addClass("active");
 });
 </script>
-<h1>home page</h1>
+<h1>HOME</h1>
 
 <section class="home_page" style="position:relative";>
-  Lorem Ipsum je fiktívny text, používaný pri návrhu tlačovín a typografie. Lorem Ipsum je štandardným výplňovým textom už od 16. storočia, keď neznámy tlačiar zobral sadzobnicu plnú tlačových znakov a pomiešal ich, aby tak vytvoril vzorkovú knihu. Prežil nielen päť storočí, ale aj skok do elektronickej sadzby, a pritom zostal v podstate nezmenený. Spopularizovaný bol v 60-tych rokoch 20.storočia, vydaním hárkov Letraset, ktoré obsahovali pasáže Lorem Ipsum, a neskôr aj publikačným softvérom ako Aldus PageMaker, ktorý obsahoval verzie Lorem Ipsum.
+  <b>Welcome to our Gastro Page !!! </b> <br>
+  On this website you can look at our weekly menu, as well as reserve table with options according to your selection. If you want to view our food options, pick option "Daily Menu" from menu bar. If you want to reserve table, click "Reservation" and let our page guide you through reservation process. If you are new customer, you might want to know where our restauration is located. If this is the case, click "Contact" option in the menu.<br>
+  <h5> Reservation process.</h5>
+  In first step you will have to pick date and time. Current date is set by default.When you are finished with your selection click on Next button.<br>
+  In second step you have the option to choose number of people that will come. Default option is one.<br>
+  In third step you can choose additional options. Smoking means if you would like to choose table in smoking area. Window means if you want to sit next to window and option Alone means if you dont want to share your table with other customers. You DONT have to select any options. <br>
+  Fourth step in the process is setting your name that will be linked with your reservation.<br>
+  After clicking next there will be short animation and summary of your reservation will be shown to you. Here you can either delete this reservation or go back to Home page. It's also possible that there are currently no tables available, which will cause pop up window to show.
+  <br>
+  Continue by selecting desired option from menu or click the button bellow to reserve.
+  <br>
 
-Lorem Ipsum je fiktívny text, používaný pri návrhu tlačovín a typografie. Lorem Ipsum je štandardným výplňovým textom už od 16. storočia, keď neznámy tlačiar zobral sadzobnicu plnú tlačových znakov a pomiešal ich, aby tak vytvoril vzorkovú knihu. Prežil nielen päť storočí, ale aj skok do elektronickej sadzby, a pritom zostal v podstate nezmenený. Spopularizovaný bol v 60-tych rokoch 20.storočia, vydaním hárkov Letraset, ktoré obsahovali pasáže Lorem Ipsum, a neskôr aj publikačným softvérom ako Aldus PageMaker, ktorý obsahoval verzie Lorem Ipsum.
+  <button class="button-nav round-div" onclick="window.location.href='/webtechnology/reservation'" style="position:relative; left: 50%; margin-top:10px;">Reservation</button>
 </section>
 
 <section class="home_page" style="position:relative";>
