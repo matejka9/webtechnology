5609a5a5f803ea60797352807797dfebd125323f
matejka9 <matejka9@uniba.sk>
2017-01-09 22:59:24 +0100

    soem fixes

--- application/views/about_view.php
+++ application/views/about_view.php
@@ -3,24 +3,24 @@
 <link href="<?= base_url()?>assets/css/style-about-big.css" rel="stylesheet" media="only screen and (min-width: 1001px)">
 
 
-<article class='about_page'>
+<article class='about_article'>
 
 	<h1>O nás</h1>
 
 	<section>
-	<h3>Náš príbeh</h3>
-	<p>Reštaurácia Tutorial 3 2016 je našou najnovšou  a už  štvrtou otvorenou pobočkou na Slovensku. Idea otvoriť si vlastnú reštauračnú sieť vznikla najmä kvôli našej rodinnej kulinárskej histórií a pýšime sa už tretou generáciou šéfkuchárov, ktorý precestovali a varili snáď  v každom kraji sveta.</p>
-	<p>Práve tieto nazbierané skúsenosti  a kombinácie toho najlepšie zo sveta s čarom slovenskej kuchyne chceme ponúknuť aj Vám, bez toho aby ste museli opustiť hranice krajiny.</p>
-	<p>Už piaty rok za sebou sme obhájili 4 hviezdičky a denne sa  tešíme z našej narastajúcej klientely a preto prichádzame ponúkať naše služby aj tu v centre Bratislavy.</p>
-	<p> S viac ako 90 ročnou rodinnou skúsenosťou a štipkou soli v dlaniach  si Vás dovoľujeme pozvať k nám na nezabudnuteľný zážitok.</p>
+	<h2>Náš príbeh</h2>
+	<p>Reštaurácia Tutorial 3 2016 je našou najnovšou a v poradí už treťou otvorenou pobočkou na Slovensku. Nápad otvoriť si vlastnú reštauračnú sieť vznikla najmä kvôli našej rodinnej kulinárskej histórií. Pýšime sa už treťou generáciou šéfkuchárov, ktorí precestovali a varili na každom kontinente sveta.</p>
+	<p>Práve tieto nazbierané skúsenosti, ako aj kombinácie toho najlepšieho zo sveta chceme ponúknuť aj Vám, bez toho aby ste museli opustiť hranice Slovenska.</p>
+	<p>Už piaty rok za sebou sme obhájili 4 hviezdičky a denne sa tešíme z našej narastajúcej klientely. Z týchto dôvodov prichádzame ponúkať naše služby aj v centre Bratislavy.</p>
+	<p>S viac ako 90 ročnou rodinnou skúsenosťou a štipkou soli v dlaniach si Vás dovoľujeme pozvať na nezabudnuteľný zážitok.</p>
 	
 
 	</section>
 
 	<section>
-	<h3> Tradícia a ocenenia </h3>
-	<p>Vašim šéfkuchárom bude svetoznámy slovenský prodigy kulinár Dušan Zagy. Pokiaľ sa pohybujete vo svete varenia toto meno Vám určite zarezonovalo v pamäti. Pokiaľ nie tu je len niekoľko ocenení, ktorými sa pýšime: "Najlepšie kura", "Best teamwork", "Top Chef" , alebo "Varíme holými rukami".</p>
-	<p>Okrem perfektnej kvality jedľa, najlepších kuchárov pod slnkom a profesionálneho týmu Vám ponúkame aj menu z ktorého si vyberie každý. Ponúkame Vám všetky druhy  mäsitých, či bezmäsitých jedál,  ryby, dezerty, polievky, alebo šaláty. Jednou z našich najviac oceňovaných stránok je pomer kvality jedľa ku cene a najmä rýchlosť našej obsluhy. Navštívte nás a presvedčte sa sami.</p>
+	<h2>Tradícia a ocenenia</h2>
+	<p>Vašim šéfkuchárom bude svetoznámy slovenský prodigy kulinár Dušan Zagy. Pokiaľ sa pohybujete vo svete varenia tak Vám toto meno Vám určite zarezonovalo v pamäti. Pokiaľ nie, tak tu je len niekoľko ocenení, ktorími sa pýšime práve vďaka našemu kuchárskemu tímu : "Najlepšie kura", "Best teamwork", "Top Chef" , alebo "Varíme holými rukami".</p>
+	<p>Okrem perfektnej kvality jedla, najlepších kuchárov pod slnkom a profesionálneho tímu, Vám ponúkame aj menu, z ktorého si vyberie každý. Ponúkame Vám všetky druhy mäsitých, či bezmäsitých jedál, ako aj ryby, dezerty, polievky, alebo šaláty. Jednou z našich najviac oceňovaných vlastností je pomer kvality jedla k cene a najmä rýchlosť našej obsluhy. Navštívte nás a presvedčte sa sami!</p>
 	<figure class='centerer'>
 	    <img class="centerer" src="<?= base_url()?>assets/images/chef.jpg" alt="chef">
 	    <figcaption class='centerer'>
@@ -30,9 +30,9 @@
 	</section>
 
 	<section>
-	<h3>Lokalita</h3>
-	<p>Poloha reštaurácie sa nachádza priamo v centre mesta na ulici Nedbalová 10. Táto tichá lokácia je ideálna pre vychutnanie si svojho jedla a rekreáciu počas náročného pracovného dňa. V prípade, že  následne plánujete turistiku po meste, neďaleko reštaurácie sa nachádza niekoľko významných historických pamiatok, ku ktorým sa dostanete peši do niekoľkých minút. Niektorými z nich sú Stará Tržnica, Primaciálny palác, Michalská veža, Dóm sv. Martina, alebo Slovenské národne divadlo.</p>
-	<p>V prípade akéhokoľvek záujmu zavolajte, alebo použite našu web stránku na prezretie si denného menu a rovno sa zarezervujte.</p>
+	<h2>Lokalita</h2>
+	<p>Poloha reštaurácie sa nachádza priamo v centre mesta na ulici Nedbalová 10. Táto tichá lokácia je ideálna pre vychutnanie si svojho jedla a rekreáciu počas náročného pracovného dňa. V prípade, že následne plánujete turistiku po meste, neďaleko reštaurácie sa nachádza niekoľko významných historických pamiatok, ku ktorím sa dostanete peši do niekoľkých minút. Niektorými z nich sú Stará Tržnica, Primaciálny palác, Michalská veža, Dóm sv. Martina, alebo Slovenské národne divadlo.</p>
+	<p>V prípade akéhokoľvek záujmu zavolajte, alebo použite našu web stránku na prezretie si denného menu.</p>
 	<figure class='centerer'>
 	    <img class="centerer" src="<?= base_url()?>assets/images/restaurant.jpg" alt="restaurant">
 	    <figcaption class='centerer'>
@@ -41,4 +41,40 @@
   	</figure>
 	</section>
 
+</article>
+
+<article class="about_article">
+
+
+  <h2>Spokojní zákazníci</h2>
+  
+  <section>
+    <figure class='centerer space'>
+  	 <img class="centerer food_picture img-responsive" src="<?= base_url()?>assets/images/businessman.jpg" alt="podnikatel">
+     <figcaption class='centerer'>
+        Zákazník: Podnikateľ Jožo
+      </figcaption>
+    </figure>
+    <p class='space'>
+      Podnikateľ Jožo je naším každodenným zákazníkom už od otvorenia. Vídame ho v našich priestoroch pravidelne, od pondelka do piatku, počas našich obedových hodín. Naša reštaurácia sa nachádza v centre mesta a denne nás navštevujú podnikatelia, ktorým ponúkame rýchlu a zdravú alternatívu fastfood-ových reštarácii. 
+    </p>
+    <p class='cit centerer'>
+      <q><i>Pokým som nepoznal reštauráciu Tutorial 3 2016, moje stravovacie návyky boli veľmi nezdravé. Vďaka ich jednoduchej a rýchlej rezervácii dostanem kvalitné jedlo bez čakania.</i></q>
+    </p>
+  </section>
+
+  <section>
+    <figure class='centerer space'>
+      <img class="centerer food_picture img-responsive" src="<?= base_url()?>assets/images/vegangirl.jpg" alt="veganka">
+      <figcaption class='centerer'>
+        Zákazník: Vegánka Janka
+      </figcaption>
+    </figure>
+    <p class='space'>
+      Naša reštaurácia ulahodí aj najkomplikovanejším stravovacím požiadavkám našich zákazníkov. Denne ponúkame štyri možnosti obedového menu, z ktorého aspoň jedna vyhovuje aj vegánskym zákazníkom.
+    </p>
+    <p class='cit centerer'>
+      <q><i>Moja životná voľba mi prináša časté problémy pri stravovaní. Reštaurácia Tutorial 3 2016 je ako darom z nebies. Každý deň ponúkajú vegánsku alternatívu obeda.</i></q>
+    </p>
+  </section>
 </article>
--- application/views/home_view.php
+++ application/views/home_view.php
@@ -10,13 +10,13 @@ $(function(){
 });
 </script>
 
-<h1>Domov</h1>
+<h1>Tutorial 3 2016</h1>
 
-<article class="home_page">
-  <h3>Vitajte na stránkach reštaurácie Tutorial 3 2016 </h3>
-  <p>Tešíme sa z Vašeho záujmu a očakávame Vás v našich priestoroch kde Vás obslúži náš profesionálny tým a jedľa Vám budú pripravované pod osobným dozorom svetoznámeho šéfkuchára Dušana Zagyho.</p>
-  <p>V reštaurácií Tutorial 3 2016 používame iba ingrediencie najväčšej kvality a čerstvosti na zostrojenie Vašeho perfektného jedla. V prípade záujmu pozrite si naše denné menu a pokiaľ Vás naša ponuka zaujme, môžete si rovno zarezervovať stôl z pohodlia Vášho domova na pár klikov.</p>
-  <p>Ak si prídete sadnúť len kvôli nápojom, nebudeme Vás súdiť - naopak sme hrdý na náš výber kokteilov , vín, pív, či džúsov podľa Vášho výberu</p>
+<article class="home_article">
+  <h2>Vitajte na stránkach reštaurácie Tutorial 3 2016 </h2>
+  <p>Tešíme sa z Vašeho záujmu a očakávame Vás v našich priestoroch kde Vás obslúži náš profesionálny tím. Jedlo Vám bude pripravované pod osobným dozorom svetoznámeho šéfkuchára Dušana Zagyho.</p>
+  <p>V reštaurácií Tutorial 3 2016 používame iba ingrediencie najvyššej kvality a čerstvosti na zostrojenie Vašeho perfektného jedla. V prípade záujmu si pozrite naše denné menu a pokiaľ Vás naša ponuka zaujme, môžete si rovno zarezervovať stôl z pohodlia Vášho domova.</p>
+  <p>Ak si prídete sadnúť len na nápoj, nebudeme Vás súdiť. Naopak sme hrdí na náš výber kokteilov , vín, pív, či džúsov podľa Vášho výberu priamo pri našom bare.</p>
  </article>
 
  <div class="round-div button button-nav box reservbut" onclick="window.location.href='/webtechnology/index.php/reservation'"  id="back">  
@@ -25,37 +25,44 @@ $(function(){
       </span>
 </div>
 
-<article class="home_page">
-  <h2>Spokojní zákazníci</h2>
-  
-  <section>
-    <figure class='centerer space'>
-  	 <img class="centerer food_picture img-responsive" src="<?= base_url()?>assets/images/businessman.jpg" alt="podnikatel">
-     <figcaption class='centerer'>
-        Zákazník: Podnikateľ Jožo
-      </figcaption>
-    </figure>
-    <p class='space'>
-      Podnikateľ Jožo je naším každodenným zákaznikom už od otvorenia. Vídame ho tu pravidelne od pondelka do piatku počas našich obedových hodín. Naša reštaurácia sa nachádza v centre mesta a denne nás navštevujú podnikatelia, ktorým ponúkame rýchlu a zdravú alternatívu fastfood-ových reštarácii. 
-    </p>
-    <p class='cit centerer'>
-      <q>Pokým som nepoznal reštauráciu Tutorial 3 2016, moje stravovacie náviky boli veľmi nezdravé. Vďaka ich jednoduchej a rýchlej rezervácii dostanem kvalitné jedlo bez čakania.</q>
-    </p>
-  </section>
+<article class="home_article">
+  <p>Tešíme sa na Vašu návštevu. Naše sídlo nájdete v záložke <a href="index.php/contact" class='link_kontakt'>Kontakt</a> v nasledujúcich hodinách:</p>
 
-  <section>
-    <figure class='centerer space'>
-      <img class="centerer food_picture img-responsive" src="<?= base_url()?>assets/images/vegangirl.jpg" alt="veganka">
-      <figcaption class='centerer'>
-        Zákazník: Vegánka Janka
-      </figcaption>
-    </figure>
-    <p class='space'>
-      Naša reštaurácia uľahodí aj najkomplikovanejším stravovacím požiadavkám naších zákaznikov. Denne ponúkame štyri možnosti obedového menu, z ktorého aspoň jedna vyhovuje aj vegánskym zákaznikom.
-    </p>
-    <p class='cit centerer'>
-      <q>Moja životná voľba mi prináša časté problémi pri stravovaní. Reštauráciu Tutorial 3 2016 je ako darom z nebies. Každý deň ponúkajú vegánsku alternatívu obeda.</q>
-    </p>
-  </section>
+  <table class='left_space bottom_space'>
+    <tr>
+      <th>Po:</th>
+      <td>&nbsp;11:00 - 15:00</td>
+    </tr>
+
+    <tr>
+      <th>Ut:</th>
+      <td>&nbsp;11:00 - 15:00</td>
+    </tr>
+
+    <tr>
+      <th>St:</th>
+      <td>&nbsp;11:00 - 15:00</td>
+    </tr>
+
+    <tr>
+      <th>Št:</th>
+      <td>&nbsp;11:00 - 15:00</td>
+    </tr>
+
+    <tr>
+      <th>Pi:</th>
+      <td>&nbsp;11:00 - 15:00</td>
+    </tr>
+
+    <tr>
+      <th>So:</th>
+      <td>&nbsp;Zatvorené</td>
+    </tr>
+
+    <tr>
+      <th>Ne:</th>
+      <td>&nbsp;Zatvorené</td>
+    </tr>
+  </table>
 </article>
 
